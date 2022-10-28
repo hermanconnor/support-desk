d42177d8ef93fdc615568df55e5d6e1f88b28521
herman <hermanconnor11@gmail.com>
2022-10-28 18:27:20 -0400

    Create user model and register controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,6 @@
+import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -6,7 +9,30 @@ const registerUser = async (req, res) => {
     throw new Error('Please enter all fields');
   }
 
-  res.send('Register Route');
+  const userExists = await User.findOne({ email }).lean().exec();
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const hashPwd = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashPwd,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data received');
+  }
 };
 
 const loginUser = async (req, res) => {
--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
 import 'express-async-errors';
+import 'colors';
+import connectDB from './config/db.js';
 import errorHandler from './middleware/errorHandler.js';
 import userRoutes from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// DATABASE CONNECTION
+connectDB();
 
 // MIDDLEWARE
 app.use(express.json());
@@ -18,4 +24,10 @@ app.use('/api/v1/users', userRoutes);
 // ERROR HANDLER
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// START SERVER
+mongoose.connection.once('open', () => {
+  console.log(
+    `Connected to MongoDB: ${mongoose.connection.host}`.cyan.underline,
+  );
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+});
