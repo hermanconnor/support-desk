537a3a52a135ca1cd191f92c4653a9ad1b665a8b
herman <hermanconnor11@gmail.com>
2022-11-16 17:25:03 -0500

    Add create ticket thunk and service

---
+++ client/src/features/notes/noteSlice.js
@@ -0,0 +1,90 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import noteService from './noteService';
+
+const initialState = {
+  notes: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+// Get Ticket Notes
+export const getNotes = createAsyncThunk(
+  'notes/getAll',
+  async (ticketId, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.accessToken;
+
+      return await noteService.getNotes(ticketId, token);
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  },
+);
+
+// Create A Ticket Note
+export const createNote = createAsyncThunk(
+  'notes/create',
+  async ({ noteText, ticketId }, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.accessToken;
+
+      return await noteService.createNote(noteText, ticketId, token);
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  },
+);
+
+export const noteSlice = createSlice({
+  name: 'note',
+  initialState,
+  reducers: {
+    notesReset: (state) => initialState,
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNotes.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getNotes.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.notes = action.payload;
+      })
+      .addCase(getNotes.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(createNote.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNote.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.notes.push(action.payload);
+      })
+      .addCase(createNote.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { notesReset } = noteSlice.actions;
+
+export default noteSlice.reducer;
--- client/src/pages/Ticket.jsx
+++ client/src/pages/Ticket.jsx
@@ -1,18 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { FaPlus } from 'react-icons/fa';
+import Modal from 'react-modal';
 import { toast } from 'react-toastify';
 
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import { getNotes, createNote } from '../features/notes/noteSlice';
+import NoteItem from '../components/NoteItem';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%,-50%)',
+    position: 'relative',
+  },
+};
+
+Modal.setAppElement('#root');
+
 const Ticket = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState('');
+
   const { ticketId } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+  const { ticket, isLoading, isError, message } = useSelector(
     (state) => state.tickets,
+  );
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes,
   );
 
   useEffect(() => {
@@ -21,6 +46,7 @@ const Ticket = () => {
     }
 
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
   }, [dispatch, isError, message, ticketId]);
 
   const handleClose = () => {
@@ -29,7 +55,23 @@ const Ticket = () => {
     navigate('/tickets');
   };
 
-  if (isLoading) return <Spinner />;
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleNoteSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createNote({ noteText, ticketId }));
+
+    closeModal();
+  };
+
+  if (isLoading || notesIsLoading) return <Spinner />;
 
   if (isError) return <h3>Something Went Wrong</h3>;
 
@@ -55,7 +97,49 @@ const Ticket = () => {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+
+        <h2>Notes</h2>
       </header>
+
+      {ticket.status !== 'closed' && (
+        <button className='btn' onClick={openModal}>
+          <FaPlus /> Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        style={customStyles}
+        onRequestClose={closeModal}
+        contentLabel='Add Note'
+      >
+        <h2>Add Note</h2>
+        <button className='btn-close' onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={handleNoteSubmit}>
+          <div className='form-group'>
+            <textarea
+              name='noteText'
+              id='noteText'
+              className='form-control'
+              placeholder='Note text'
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+
+          <div className='form-group'>
+            <button type='submit' className='btn'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
 
       {ticket.status !== 'closed' && (
         <button className='btn btn-block btn-danger' onClick={handleClose}>
